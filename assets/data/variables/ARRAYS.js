afdd0d0057ec573c0c971073320c411949c932e9
Parker <pp101socials@gmail.com>
2023-07-15 08:02:20 -0400

    Created arrays

--- assets/data/variables/ARRAYS.js
+++ assets/data/variables/ARRAYS.js
@@ -2,6 +2,34 @@
 
 import { REVIEW_1, REVIEW_2, REVIEW_3, REVIEW_4 } from "./OBJECTS";
 
+const INDEX_KWS = [
+  "radiance glow hsc",
+  "radiance glow hard surface cleaning",
+  "radiance glow",
+  "chip holden",
+  "north carolina cleaning",
+  "nc pressure washing",
+  "nc tile and grout",
+  "nc carpet cleaning",
+  "nc upholstery cleaning",
+];
+const ABOUT_KWS = [
+  "about radiance glow hsc",
+  "about radiance glow hard surface cleaning",
+  "about chip holden",
+  "about radiance glow",
+  "radiance glow hsc background",
+  "radiance glow hsc reviews",
+];
+const CONTACT_KWS = [
+  "contact radiance glow hsc",
+  "contact radiance glow hard surface cleaning",
+  "book a service",
+  "book an appointment",
+  "contact chip holden",
+];
+
 const REVIEWS = [REVIEW_1, REVIEW_2, REVIEW_3, REVIEW_4];
 
+export { INDEX_KWS, ABOUT_KWS, CONTACT_KWS };
 export { REVIEWS };
