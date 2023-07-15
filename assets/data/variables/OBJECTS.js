f872e563ffae91a3f99be4e5e72c8bcb7ade9167
Parker <pp101socials@gmail.com>
2023-07-15 08:02:32 -0400

    Created objects

--- assets/data/variables/OBJECTS.js
+++ assets/data/variables/OBJECTS.js
@@ -9,25 +9,25 @@ import {
 
 import Review from "../classes/Review";
 
-const REVIEW_1 = Review(
+const REVIEW_1 = new Review(
   "R_1",
   PFP_REVIEW_1,
   "Nick Urbina",
   "Radiance Carpet cleaning will be the only company that comes to my home from now on."
 );
-const REVIEW_2 = Review(
+const REVIEW_2 = new Review(
   "R_2",
   PFP_REVIEW_2,
   "Daniela Trias",
   "Chip did an excellent work cleaning the tiles of my house, especially the kitchen area."
 );
-const REVIEW_3 = Review(
+const REVIEW_3 = new Review(
   "R_3",
   PFP_REVIEW_3,
   "Dale Carter",
   "Chip will treat your house like it's his own. This is my third time using his services in the last 3 years."
 );
-const REVIEW_4 = Review(
+const REVIEW_4 = new Review(
   "R_4",
   PFP_REVIEW_4,
   "Sharon Campbell Fields",
