daa1043621a2472829c5bc675e3f2fb51ac1e9ca
Parker <pp101socials@gmail.com>
2023-07-15 11:47:00 -0400

    Added components

--- pages/404.js
+++ pages/404.js
@@ -10,6 +10,8 @@ import { useRouter } from "next/router";
 import { PageHead } from "@/assets/components/global/All/PageHead";
 import { NavTop } from "@/assets/components/global/Nav/Both/NavTop";
 import { DesktopNav } from "@/assets/components/global/Nav/Desktop/DesktopNav";
+import { MobileNav } from "@/assets/components/global/Nav/Mobile/MobileNav";
+import { MobileNavMenu } from "@/assets/components/global/Nav/Mobile/MobileNavMenu";
 
 // Style Imports
 import "../assets/styles/modules/404/404.module.css";
@@ -20,10 +22,12 @@ export default function NotFound() {
   return (
     <div id="PAGE" className="overrides_404 page full-second">
       <PageHead />
+      <MobileNavMenu disableLink="/404" />
 
       <main id="PAGE_CNT">
         <NavTop />
         <DesktopNav disableLink="/404" />
+        <MobileNav disableLink="/404" />
       </main>
     </div>
   );
--- pages/about.js
+++ pages/about.js
@@ -10,6 +10,8 @@ import { useRouter } from "next/router";
 import { PageHead } from "@/assets/components/global/All/PageHead";
 import { NavTop } from "@/assets/components/global/Nav/Both/NavTop";
 import { DesktopNav } from "@/assets/components/global/Nav/Desktop/DesktopNav";
+import { MobileNav } from "@/assets/components/global/Nav/Mobile/MobileNav";
+import { MobileNavMenu } from "@/assets/components/global/Nav/Mobile/MobileNavMenu";
 
 // Style Imports
 import "../assets/styles/modules/About/About.module.css";
@@ -20,10 +22,12 @@ export default function About() {
   return (
     <div id="PAGE" className="overrides_About page full-second">
       <PageHead />
+      <MobileNavMenu disableLink="/about" />
 
       <main id="PAGE_CNT">
         <NavTop />
         <DesktopNav disableLink="/about" />
+        <MobileNav disableLink="/about" />
       </main>
     </div>
   );
