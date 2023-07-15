8b946a9a9bd410e81cc51de7f4b545e1d1491d1d
Parker <pp101socials@gmail.com>
2023-07-15 08:50:55 -0400

    Created file

--- assets/components/global/Nav/Desktop/DesktopNav.js
+++ assets/components/global/Nav/Desktop/DesktopNav.js
@@ -4,8 +4,88 @@
  *
  */
 
+import { LOGO } from "@/assets/cdns/CDNIcons";
+
 import styles from "../../../../styles/modules/Nav/Nav.module.css";
 
 export const DesktopNav = (props) => {
-  return "";
+  return (
+    <nav
+      id="desktopNav"
+      className={`${styles.desktop_nav} overrides_DesktopNav fm-motion full-second`}
+    >
+      <div className={`${styles.desktop_nav_inner}`}>
+        <div className={`${styles.desktop_nav_inner_box} container-fluid`}>
+          <div className={`${styles.desktop_nav_inner_row} row`}>
+            <div
+              className={`${styles.desktop_nav_inner_side} ${styles.desktop_nav_L} col-lg-4 col-md-4 col-sm-4 col-xs-4`}
+            >
+              <div className={`${styles.desktop_nav_inner_side_cnt}`}>
+                {props.disableLink === "/" ? (
+                  <div className={`${styles.logo} ${styles.deactive}`}>
+                    <img
+                      data-src={LOGO}
+                      className="lazyload"
+                      alt="Radiance Glow HSC Logo."
+                    />
+                  </div>
+                ) : (
+                  <div className={`${styles.logo} ${styles.active}`}>
+                    <a href="/">
+                      <img
+                        data-src={LOGO}
+                        className="lazyload"
+                        alt="Radiance Glow HSC Logo."
+                      />
+                    </a>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div
+              className={`${styles.desktop_nav_inner_side} ${styles.desktop_nav_R} col-lg-8 col-md-8 col-sm-8 col-xs-8`}
+            >
+              <div className={`${styles.desktop_nav_inner_side_cnt}`}>
+                <ul>
+                  {props.disableLink === "/" ? (
+                    <li className={`${styles.deactive}`}>
+                      <span>HOME</span>
+                    </li>
+                  ) : (
+                    <li className={`${styles.active}`}>
+                      <a href="/">
+                        <span>HOME</span>
+                      </a>
+                    </li>
+                  )}
+                  {props.disableLink === "/about" ? (
+                    <li className={`${styles.deactive}`}>
+                      <span>ABOUT</span>
+                    </li>
+                  ) : (
+                    <li className={`${styles.active}`}>
+                      <a href="/about">
+                        <span>ABOUT</span>
+                      </a>
+                    </li>
+                  )}
+                  {props.disableLink === "/contact" ? (
+                    <li className={`${styles.deactive}`}>
+                      <span>CONTACT</span>
+                    </li>
+                  ) : (
+                    <li className={`${styles.active}`}>
+                      <a href="/contact">
+                        <span>CONTACT</span>
+                      </a>
+                    </li>
+                  )}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
