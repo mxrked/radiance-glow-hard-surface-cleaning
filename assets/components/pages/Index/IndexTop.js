d65a2bf3c36fa72f453805b63ed0144cc1a725ab
Parker <pp101socials@gmail.com>
2023-07-16 09:28:54 -0400

    Created file

---
+++ assets/components/pages/Index/IndexTop.js
@@ -0,0 +1,59 @@
+/**
+ *
+ *  This is the Index Top
+ *
+ */
+
+import { BackgroundImage } from "react-image-and-background-image-fade";
+
+import { INDEX_TOP_BG } from "@/assets/cdns/CDNBgs";
+
+import styles from "../../../styles/modules/Index/Index.module.css";
+
+export const IndexTop = () => {
+  return (
+    <section
+      id="indexTop"
+      className={`${styles.index_top} overrides_IndexTop fm-motion full-second`}
+    >
+      <BackgroundImage
+        src={INDEX_TOP_BG}
+        width="100%"
+        height="100%"
+        className={`${styles.bg}`}
+      />
+
+      <div className={`${styles.overlay}`}>
+        <div className={`${styles.overlay_cnt}`}>
+          <h1 className="orientation-change-element half-second">
+            Clean with a <br />
+            Radiance-like Glow.
+          </h1>
+
+          <p className="orientation-change-element half-second">
+            A local NC cleaning service which includes:
+            <br />
+            Pressure-washing, tile & grout cleaning, carpet cleaning and
+            upholstery cleaning.
+          </p>
+
+          <div className={`${styles.overlay_cnt_links}`}>
+            <a
+              href="/about"
+              className={`${styles.about_link} orientation-change-element half-second`}
+            >
+              <span>About RGHSC</span>
+            </a>
+
+            <a
+              href="/contact"
+              className={`${styles.contact_link} orientation-change-element half-second`}
+            >
+              <span>Book A Service</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
