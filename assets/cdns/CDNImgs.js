00104bfaa2046370980243a76ba97f53bfcf4002
Parker <pp101socials@gmail.com>
2023-10-27 09:53:38 -0400

    Added imgs

--- assets/cdns/CDNImgs.js
+++ assets/cdns/CDNImgs.js
@@ -14,5 +14,9 @@ const PFP_REVIEW_2 = CDNImgReturn("pfps", "review-2", "webp");
 const PFP_REVIEW_3 = CDNImgReturn("pfps", "review-3", "webp");
 const PFP_REVIEW_4 = CDNImgReturn("pfps", "review-4", "webp");
 
+const EXPERTISE_LOGO = CDNImgReturn("logos", "expertise", "webp");
+const BUSINESS_UP_LOGO = CDNImgReturn("logos", "business-up", "webp");
+
 export { BROCHURE_BUSINESS, BROCHURE_RESIDENTAL };
 export { PFP_REVIEW_1, PFP_REVIEW_2, PFP_REVIEW_3, PFP_REVIEW_4 };
+export { EXPERTISE_LOGO, BUSINESS_UP_LOGO };
